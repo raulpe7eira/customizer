a09198651bbc4be966b98e2c6489910654a72d24
Raul Pereira <raul.pereira@gmail.com>
2017-03-21 21:37:48 -0300

    Home page created

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'home#index'
+
   resources :custom_fields
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
